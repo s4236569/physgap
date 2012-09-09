fb5bd03adc28f0f77e8584bff04a2abd102b042a
Michael McClintock <michael.ross.mcclintock@gmail.com>
2012-09-09 22:40:59 +1000

    trying turning of extensions

--- scripts/mathjax/config/default.js
+++ scripts/mathjax/config/default.js
@@ -206,8 +206,8 @@ MathJax.Hub.Config({
   //  have been moved to this more central location since they are shared
   //  by all output jax.
   //
-  showMathMenu: true,
-  showMathMenuMSIE: true,
+  showMathMenu: false,
+  showMathMenuMSIE: false,
 
 
   //
